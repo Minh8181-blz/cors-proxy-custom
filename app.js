68defbec63f79c5a0387d82df3cd459a11a87be6
MinhPC8181Home <vuongminh8181@gmail.com>
2023-04-04 21:12:13 +0700

    add ping path

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+  if (req.method === 'GET' && req.path === "/ping") {
+    res.send("ok");
+    return;
+  }
 
   if (req.method === 'OPTIONS') {
       // CORS Preflight
